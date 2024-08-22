48cebf872e07733ded1d522d72f00aa7d0c66624
Trinh Nguyen <49252874+trinhxuann@users.noreply.github.com>
2024-08-22 16:44:36 -0700

    Update schemaJoin.R
    Joining via the provided schema now should work. Rules were generated to ensure programmatic joins using the relationship table. They are:
    
    1. Begin with an edge table that is also a primary table
    2. Focus only on tables with cascade update or delete ability as these are non-lookup tables
    3. Begin with 1:1 relationships
    4. Continue joins via 1:many relationship until completion.
    
    This has been tested across several databases. Due to rule 2 above, look up tables are not automatically joined. Users will have to join these themselves.
    
    Code not 100% optimized but working.

--- R/schemaJoin.R
+++ R/schemaJoin.R
@@ -5,55 +5,105 @@
 #' @param verbose T/F, a logical argument to print the relationship within the
 #' schema or not.
 #'
-#' @return A table of the schema without the system tables.
+#' @return A schema without the system tables and translated column names.
 #'
 #' @importFrom stats aggregate
 #' @noRd
 #' @keywords internal
-translateSchema <- function(schema, verbose = F) {
+translateSchema <- function(schema, verbose = FALSE) {
+  # Filter out MSys relationships
+  schema <- schema[!grepl("^MSys.*", schema$szRelationship), ]
 
-  filteredData <- schema[!grepl("^MSys.*", schema[["szRelationship"]]), ]
+  # Aggregate columns based on relationship details
+  # Need this for relationships with multiple keys
+  schema <- aggregate(cbind(szColumn, szReferencedColumn) ~ grbit + szObject + szReferencedObject + szRelationship,
+                      data = schema, FUN = paste, collapse = ".")
 
-  if (is.null(schema$joinType)) {
-    groupedData <- aggregate(cbind(szColumn, szReferencedColumn) ~ grbit + szObject + szReferencedObject + szRelationship,
-                             data = filteredData, FUN = paste, collapse = ".")
-  } else {
-    groupedData <- aggregate(cbind(szColumn, szReferencedColumn) ~ grbit + szObject + szReferencedObject +
-                               szRelationship + joinType,
-                             data = filteredData, FUN = paste, collapse = ".")
-  }
+  # Rename columns for clarity
+  names(schema)[match(c("szColumn", "szReferencedColumn", "szObject", "szReferencedObject"),
+                      names(schema))] <- c("foreignKeys", "primaryKeys", "foreignTable", "primaryTable")
 
-  names(groupedData)[which(names(groupedData) %in% c("szColumn", "szReferencedColumn"))] <- c("foreignKeys", "primaryKeys")
-  groupedData$szRelationship <- factor(groupedData$szRelationship, levels = unique(groupedData$szRelationship))
-  schema <- groupedData[order(groupedData$szRelationship), ]
+  # Order by relationship name
+  # schema <- schema[order(schema$szRelationship), ]
 
-  if (is.null(schema$joinType)) {
-    if (any(tolower(names(schema)) %in% "grbit")) {
-      schema$joinType <- sapply(schema$grbit, function(x) {
-        if (x < 16777216) "inner_join"
-        else (if (x >= 16777216 & x < 33554432) "left_join"
-              else ("right_join"))
-      })
-    }
-  }
+  # Determine join type based on grbit value
+  schema$joinType <- ifelse(schema$grbit < 16777216, "inner_join",
+                            ifelse(schema$grbit < 33554432, "left_join", "right_join"))
 
-  schema$joinFunction <- sapply(schema$joinType, function(x) {
-    if (x == "inner_join") function(a, b, by) merge(a, b, by, all = F)
-    else (if (x == "left_join") function(a, b, by) merge(a, b, by, all.x = T)
-          else (if (x == "right_join") function(a, b, by) merge(a, b, by, all.y = T)
-                else (if (x == "full_join") function(a, b, by) merge(a, b, by, all = T)
-                      else(stop("Supply joinType as `inner_join`, `left_join`, or `right_join` only.", call. = F)))))
+  # Create join function based on join type
+  schema$joinFunction <- lapply(schema$joinType, function(x) {
+    switch(x,
+           inner_join = function(x, y, by.x, by.y) merge(x, y, by.x = by.x, by.y = by.y, all = FALSE),
+           left_join = function(x, y, by.x, by.y) merge(x, y, by.x = by.x, by.y = by.y, all.x = TRUE),
+           right_join = function(x, y, by.x, by.y) merge(x, y, by.x = by.x, by.y = by.y, all.y = TRUE),
+           stop("Supply joinType as `inner_join`, `left_join`, or `right_join` only.", call. = FALSE))
   })
 
-  if (isTRUE(verbose)) {
+  # Print verbose output if requested
+  if (verbose) {
     for (i in 1:nrow(schema)) {
-      cat(schema$joinType[[i]], sQuote(schema$szReferencedObject[[i]]), "with", sQuote(schema$szObject[[i]]), "via columns", sQuote(schema$primaryKeys[[i]]), "and", sQuote(schema$foreignKeys[[i]]), "\n")
+      cat(schema$joinType[[i]], sQuote(schema$szReferencedObject[[i]]), "with", sQuote(schema$szObject[[i]]),
+          "via columns", sQuote(schema$primaryKeys[[i]]), "and", sQuote(schema$foreignKeys[[i]]), "\n")
     }
-    return(cat("\nALL tables within this relationship are:", paste(unique(c(schema$szObject, schema$szReferencedObject)),
-                                                                   collapse = ", ")))
+    cat("\nALL tables within this relationship are:", paste(unique(c(schema$szObject, schema$szReferencedObject)), collapse = ", "), "\n")
   }
 
-  schema
+  return(schema)
+}
+
+#' Determine joining by ordering the schema
+#'
+#' @param schema A schema table that mirrors that structure of an Access schema.
+#' @param providedTables A named vector of the provided relational tables
+#'
+#' @return A schema ordered in the joining order
+#'
+#' @noRd
+#' @keywords internal
+orderSchema <- function(schema, providedTables) {
+
+  # Keep only tables that are provided
+  filteredSchema <- schema[schema[["foreignTable"]] %in% providedTables & schema[["primaryTable"]] %in% providedTables, ]
+
+  # Start with tables that have the least number of connections
+  occurrences <- sort(table(c(filteredSchema[["primaryTable"]], filteredSchema[["foreignTable"]])))
+  # Start with an edge table, that is a primary table, and forces cascade update/deletes
+  # When grbit is 1, that's a 1:1 relationship in which both are primary tables
+  oneToOne <- c(1, 3, 257, 4097, 5353, 16777217, 16777453, 16781313,
+                16781569, 33554433, 33554689, 33558529, 33559785)
+  oneToOneTables <- filteredSchema[which(filteredSchema$grbit %in% oneToOne), ]
+  primaryTables <- unique(c(oneToOneTables$foreignTable, oneToOneTables$primaryTable, filteredSchema$primaryTable))
+  edgeTables <- names(occurrences[which(occurrences == 1)])
+  cascadeCandidates <- unlist(filteredSchema[which(filteredSchema$grbit > 2), c("foreignTable", "primaryTable")],
+                              use.names = F)
+  qualifyingVectors <- list(primaryTables, edgeTables, cascadeCandidates)
+  startTable <- Reduce(intersect, qualifyingVectors)
+
+  # Reorder the schema
+  # May break if the starting table is a foreign table...
+  start <- filteredSchema[filteredSchema$primaryTable == startTable |
+                            filteredSchema$foreignTable == startTable, ]
+  usedTables <- startTable
+
+  while (nrow(start) > 0) {
+    if (!start$grbit[nrow(start)] %in% oneToOne) {
+      nextTable <- filteredSchema[filteredSchema$primaryTable == start$foreignTable[nrow(start)], ]
+    } else {
+      nextTable <- filteredSchema[filteredSchema$primaryTable == start$primaryTable[nrow(start)], ]
+      if (nrow(nextTable) > 1) {
+        # Occurs if there was a one-to-one and a table is listed as primary across 1+ relationships
+        nextTable <- nextTable[!nextTable$grbit %in% oneToOne, ]
+      }
+    }
+
+    if (nrow(nextTable) > 0 && !(nextTable$primaryTable %in% usedTables)) {
+      start <- rbind(start, nextTable)
+      usedTables <- c(usedTables, nextTable$primaryTable)
+    } else {
+      break  # Exit the loop if no more related tables or if a cycle is detected
+    }
+  }
+  start
 }
 
 #' Applies the joins in accordance to a relationship schema.
@@ -67,8 +117,6 @@ translateSchema <- function(schema, verbose = F) {
 #'
 #' @param schema A schema table that mirrors the structure of an Access schema.
 #' @param data A list of data tables to be joined
-#' @param start The starting relational table to begin construction. If NULL,
-#' will start at the first table specified by the schema.
 #'
 #' @details
 #' The join type in the relationship table from Access can be modified directly if required. Simply add a `joinType` column to the relationship table and specify one of four options: `full_join`, `inner_join`, `left_join`, or `right_join`. The `data` argument can theoretically take in any data frame. This allows for manipulations of the relational tables if such operations are necessary before joining. Within the schema table itself, `szReferencedObject` refers to the relational table with the primary key, while `szObject` refers to the relational table with the foreign key.
@@ -95,65 +143,50 @@ translateSchema <- function(schema, verbose = F) {
 #' The tables in the 20mm dataset aren't sequentially linked. There are tables that
 #' are linked together first before being linked back to the original. How do I solve this?
 #'
-schemaJoin <- function(schema, data, start = NULL) {
+schemaJoin <- function(schema, data) {
+  # Validate schema
+  if (!is.data.frame(schema)) stop("Your schema is not provided as a data.frame.", call. = FALSE)
 
-  if (class(schema) == "list") stop("Your schema is not provided as a data.frame.", call. = F)
-
+  # Translate schema
   schema <- translateSchema(schema)
-  schema <- schema[-which(schema$grbit == 2), ]
-  allTables <- factor(unique(c(schema$szObject, schema$szReferencedObject)),
-                      levels = unique(c(unique(schema[["szReferencedObject"]]), unique(schema$szObject))))
 
-  tableEssentialDFs <- data.frame(
-    table = allTables,
-    count = (allTables %in% schema$szObject) + (allTables %in% schema$szReferencedObject)
-  )
-  tableEssentialDFs <- tableEssentialDFs[order(tableEssentialDFs$table), ]
-  tableEssentialDFs <- tableEssentialDFs[1:max(which(tableEssentialDFs[["table"]] %in% names(data))), ]
-  tableEssentialDFs$providedTables <- tableEssentialDFs[["table"]] %in% names(data)
+  # Number of occurrences of each relational table
+  numberOccurence <- names(sort(table(c(schema[["primaryTable"]], schema[["foreignTable"]])), decreasing = T))
 
-  tableRequired <- tableEssentialDFs[tableEssentialDFs[["count"]] == 2 & !tableEssentialDFs[["providedTables"]], "table"]
+  # Filter out relationships with grbit value of 2; these are generally
+  # look up tables that do not enforce the relationship of the database
+  schema <- schema[schema$grbit != 2, ]
 
-  if (length(tableRequired) > 0) {
-    stop("You are missing intermediate table(s) required for completing the joins: ", paste(tableRequired, collapse = ", "), call. = F)
-  }
-  schema$score <- schema$szObject %in% names(data) + schema$szReferencedObject %in% names(data)
-  schema <- schema[schema[["score"]] == 2, ]
+  # Filter schema based on available tables
+  providedTables <- names(data)
+  filteredSchema <- orderSchema(schema, providedTables)
 
-  startTable <- data[[schema$szReferencedObject[1]]]
-  usedTable <- schema$szReferencedObject[1]
+  # Start with the first referenced table
+  startTable <- data[[filteredSchema[["primaryTable"]][1]]]
+  usedTables <- filteredSchema[["primaryTable"]][[1]]
 
-  for (i in 1:nrow(schema)) {
-    # If matches table already used, use the non-parent table
-    result <- tryCatch({
-      if (any(schema$szObject[[i]] %in% usedTable)) {
-        xTable <- schema$szObject[i]
-        yTable <- schema$szReferencedObject[i]
-        xName <- strsplit(schema$foreignKeys[[i]], "\\.")[[1]]
-        yName <- strsplit(schema$primaryKeys[[i]], "\\.")[[1]]
-      } else {
-        xTable <- schema$szReferencedObject[i]
-        yTable <- schema$szObject[i]
-        xName <- strsplit(schema$primaryKeys[[i]], "\\.")[[1]]
-        yName <- strsplit(schema$foreignKeys[[i]], "\\.")[[1]]
-      }
-      cat(schema$joinType[[i]], sQuote(xTable), "with", sQuote(yTable), "via columns", sQuote(xName), "and", sQuote(yName), "\n")
+  for (i in 1:nrow(filteredSchema)) {
+    a <- filteredSchema[["primaryTable"]][[i]]
+    b <- filteredSchema[["foreignTable"]][[i]]
+    primaryKey <- strsplit(filteredSchema[["primaryKeys"]][[i]], "\\.")[[1]]
+    foreignKey <- strsplit(filteredSchema[["foreignKeys"]][[i]], "\\.")[[1]]
 
-      startTable <- schema$joinFunction[[i]](startTable,
-                                             data[[yTable]],
-                                             by = setNames(yName, xName))
-      usedTable <- c(usedTable, yTable)
-    },
-    error = function(err) {
-      if (grepl("cannot allocate vector of size", err)) {
-        return("sizeError")
-      }
+    cat(filteredSchema$joinType[[i]], sQuote(a), "with", sQuote(b), "via columns", sQuote(primaryKey), "and", sQuote(foreignKey), "\n")
+    # If the primary table has already been joined, then use the join table as the primary table
+    if (a %in% usedTables) {
+      startTable <- filteredSchema$joinFunction[[i]](
+        startTable, data[[b]],
+        by.x = primaryKey, by.y = foreignKey
+      )
+    } else {
+      startTable <- filteredSchema$joinFunction[[i]](
+        data[[a]], startTable,
+        by.x = primaryKey, by.y = foreignKey
+      )
     }
-    )
 
-    if (result[[1]] %in% "sizeError") {
-      stop("Resulting table is size is too large. Pare down your list of tables.", call. = F)
-    }
+    # Update the usedTables to account for what table have joined already
+    usedTables <- c(usedTables, b)
   }
-  startTable
+  return(startTable)
 }
