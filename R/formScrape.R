0959cd7493fe33d19df1b19750101df0ad1a7ff0
Trinh Nguyen <trinh.nguyen@wildlife.ca.gov>
2026-06-05 16:40:44 -0700

    scrapeForm example, author list

--- R/formScrape.R
+++ R/formScrape.R
@@ -115,7 +115,7 @@ uniqueNames <- function(x) {
 #' # Find available fields and their options on this webpage:
 #' scrapeForm("https://www.cbr.washington.edu/sacramento/data/query_river_graph.html)"
 #'
-#' #' # This specific query has duplicated field names across the checkboxes and a
+#' # This specific query has duplicated field names across the checkboxes and a
 #' # select field. The function creates a generic label to make these unique,
 #' # however, these are uninformative labels. To know which ones you want,
 #' # you can look at the website or the form itself. Here, the latter works
